a9b6e6137ad7fbc2c2cef5e68cb80885351df3d7
Logan Trujillo <logantrujillo@Logans-MacBook-Air.local>
2019-01-25 09:12:27 -0700

    created a table and planted seeds through migrations, as well as connected knex

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const env = 'development';
+const config = require('./knexfile.js')[env];
+const knex = require('knex')(config);
+
+app.get('/', (req, res) => {
+    knex.select('*').from('methods')
+    .then((rows) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      next(err);
+    });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
